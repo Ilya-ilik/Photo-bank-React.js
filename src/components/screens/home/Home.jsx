459f86ac3a7fec3fe90bd832516ab7b2dcc532eb
Ilya-ilik <101934601+Ilya-ilik@users.noreply.github.com>
2023-03-19 17:29:18 +0300

    sketch content component

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,30 +1,70 @@
-import styles from "./Home.module.css";
+import styles from "./Home.module.css"; // импортируем модуль css
+import { images } from "./images.data"; // импортируем данные
 
 const Header = () => {
-    return (
-      <>
-        <div className={styles.header}>
-          <span class={styles.logo} title=""/>
-          <div class={styles.rigt_panel}>
-            <nav class={styles.header__nav}>
-              <a class={styles.header__nav_link} href="/search/image">Поиск</a>
-              <a class={styles.header__nav_link} href="/galleries">Подборки</a>
-              <a class={styles.header__nav_link} href="/pricing">Наши цены</a>
-              <a class={styles.header__nav_link} href="/about">О нас</a>
-            </nav>
-            <div class={styles.login_link}>
-              <a href="/signin" class={styles.link}>Войти</a>
-            </div>
+  return (
+    <>
+      <div className={styles.header}>
+        <span className={styles.logo} title="" />
+        <div className={styles.rigt_panel}>
+          <nav className={styles.header__nav}>
+            <a className={styles.header__nav_link} href="/search/image">
+              Поиск
+            </a>
+            <a className={styles.header__nav_link} href="/galleries">
+              Подборки
+            </a>
+            <a className={styles.header__nav_link} href="/pricing">
+              Наши цены
+            </a>
+            <a className={styles.header__nav_link} href="/about">
+              О нас
+            </a>
+          </nav>
+          <div className={styles.login_link}>
+            <a href="/signin" className={styles.link}>
+              Войти
+            </a>
           </div>
         </div>
-      </>
-    );
-  };
+      </div>
+    </>
+  );
+};
+
+const Content = () => {
+  return (
+    <>
+      <div className={styles.content}>
+        <h2 className={styles.page_header}>ИЗБРАННЫЕ ФОТОГРАФИИ</h2>
+        <p className={styles.page_subheader}>
+          Оцените тематические коллекции, тщательно собранные нашими
+          фоторедакторами
+        </p>
+        <div className={styles.editor_selections_items}>
+          {images.map((img) => (
+            <div key={img.id} className={styles.editor_selections_item}>
+              <div
+                className={styles.image}
+                style={{
+                  backgroundImage: `url(${img.image})`, // Получаем в фон картинку из нашей мини базы
+                }}
+              />
+              <h2>{img.name}</h2>
+              <p>XXX изображений</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
 const Home = () => {
   return (
     <>
-     <Header/>
+      <Header />
+      <Content />
     </>
   );
 };
---
+++ src/components/screens/home/images.data.js
@@ -0,0 +1,36 @@
+export const images = [
+  {
+    id: 1,
+    name: "Новый год",
+    image:
+      "https://placepic.ru/wp-content/uploads/2018/11/nastol.com_.ua-259469.jpg",
+  },
+  {
+    id: 2,
+    name: "Зима",
+    image:
+      "https://w-dog.ru/wallpapers/10/0/305838966598971/sneg-utrom-zima-utro-goluboe-nebo-derevya-sneg.jpg",
+  },
+  {
+    id: 3,
+    name: "Рождество",
+    image:
+      "https://wp-s.ru/wallpapers/2/73/357055760387995/dom-s-kaminom-i-novogodnej-lkoj.jpg",
+  },
+  {
+    id: 4,
+    name: "Курорты",
+    image:
+      "https://vsegda-pomnim.com/uploads/posts/2022-04/1651061431_3-vsegda-pomnim-com-p-more-kurort-foto-5.jpg ",
+  },
+  {
+    id: 5,
+    name: "Архитектурные детали",
+    image: "https://kamnemir.ru/images/cms/data/projects/gallery/box/vaz4.jpg ",
+  },
+  {
+    id: 6,
+    name: "Снегопад",
+    image: "https://photocentra.ru/images/main108/1084423_main.jpg ",
+  },
+];
